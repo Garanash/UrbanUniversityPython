49bf01141e55d20ae7d2eaea94e33a32a3460d04
Andrew Michaelson <90028419+Garanash@users.noreply.github.com>
2024-10-29 12:01:28 +0300

    12 module

---
+++ module_12/suite_12_3.py
@@ -0,0 +1,10 @@
+import unittest
+
+from tests_12_3 import RunnerTest, TournamentTest
+
+ttr_1 = unittest.TextTestRunner(verbosity=2)
+runner_suite = unittest.TestSuite()
+runner_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
+runner_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
+
+ttr_1.run(runner_suite)
